bcf097c71b18aa61ab15d60bbb51095cb594ce9f
Long Bao <longbao128@gmail.com>
2019-07-22 13:13:34 +0700

    Update README and code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # dns-update-cloudflare
+Update server's IP public to DNS Record on Cloudflare.
+
+Usage: 
+```bash
+python dns_update.py <Cloudflare Email> <API Token> <Domain Name>
+```
+
+Example:
+```bash
+python3 dns_update.py abc@gmail.com 1b402587b2467862346a76ed786df abc.com
+```
---
+++ dns_update.py
@@ -0,0 +1,45 @@
+import requests as rq
+import sys
+import json
+
+domain = ''
+ip = rq.get('http://ipconfig.io/ip').text.split('\n')[0]
+url = r'https://api.cloudflare.com/client/v4/zones/'
+headers = {'X-Auth-Email': None, 'X-Auth-Key': None, 'Content-Type': 'application/json'}
+data = {'type': 'A', 'name': None, 'content': None}
+
+if len(sys.argv) == 4:
+    headers['X-Auth-Email'] = sys.argv[1]
+    headers['X-Auth-Key'] = sys.argv[2]
+    domain = sys.argv[3]
+    data['name'] = domain
+    data['content'] = ip
+else:
+    print("Usage: {} <email> <api token> <domain>".format(sys.argv[0]))
+    sys.exit(1)
+
+request = rq.get(url=url, headers=headers)
+
+if request.status_code != 200:
+    for err in request.json()['errors']:
+        print(err['message'])
+        sys.exit(1)
+else:
+    result = json.loads(request.text)['result']
+
+    for each in result:
+        if each['name'] in domain:
+            request = rq.get(url=url+r'/'+str(each['id'])+r'/dns_records', headers=headers)
+            for dm in json.loads(request.text)['result']:
+                if dm['name'] == domain:
+                    request = rq.put(url=url+r'/'+str(each['id'])+r'/dns_records/'+str(dm['id']), data=json.dumps(data)
+                                     , headers=headers)
+                    if request.status_code == 200:
+                        print("Update DNS record successfully")
+                        sys.exit(1)
+            else:
+                print("The domain can not be found")
+        else:
+            print("Token can not access to domain {}".format(domain))
+
+
